8ed4df8517ef8213d6ee3b8751bc88e030265283
Raylan Araujo <raylanra@gmail.com>
2025-08-07 06:17:16 -0300

    corrigido exemple e criado schema CT

--- workout_api/atleta/schemas.py
+++ workout_api/atleta/schemas.py
@@ -4,10 +4,10 @@ from pydantic import Field, PositiveFloat
 from workout_api.contrib.schemas import BaseSchema
 
 class Atleta(BaseSchema):
-    nome: Annotated[str, Field(str, description='Nome do atleta', examples='João', max_length=50)]
-    cpf: Annotated[str, Field(str, description='CPF do atleta', examples='12345678910', max_length=11)]
-    idade: Annotated[str, Field(int, description='Idade do atleta', examples=25)]
-    nome: Annotated[PositiveFloat, Field(description='Peso do atleta', examples=75.5)]
-    nome: Annotated[PositiveFloat, Field(description='Altura do atleta', examples=1.70)]
-    sexo: Annotated[str, Field(str, description='Sexo do atleta', examples='M', max_length=1)]
+    nome: Annotated[str, Field(str, description='Nome do atleta', example='João', max_length=50)]
+    cpf: Annotated[str, Field(str, description='CPF do atleta', example='12345678910', max_length=11)]
+    idade: Annotated[str, Field(int, description='Idade do atleta', example=25)]
+    nome: Annotated[PositiveFloat, Field(description='Peso do atleta', example=75.5)]
+    nome: Annotated[PositiveFloat, Field(description='Altura do atleta', example=1.70)]
+    sexo: Annotated[str, Field(str, description='Sexo do atleta', example='M', max_length=1)]
     
